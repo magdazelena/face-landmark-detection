c2d1af1c6d94b294e74f32636e464d442af277e5
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-01-27 10:50:58 +0100

    setup: webcam, canvas, deps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
   return (
     <div>
-
+      <Webcam ref={webcamRef} style={fullScreenStyles} />
+      <canvas ref={canvasRef} style={fullScreenStyles} />
     </div>
   );
 }
 
 export default App;
+
+const fullScreenStyles = {
+  position: "absolute",
+  width: "100vw",
+  height: "100vh",
+  top: 0,
+  left: 0,
+  right: 0,
+};

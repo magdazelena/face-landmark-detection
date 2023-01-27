ad0e232721451362780a1d2d543c39b3ac548793
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-01-27 12:22:01 +0100

    feat: face landmark detection using facemesh

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 
+const inputResolution = { width: 640, height: 480 };
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const runFacemesh = async () => {
+    const net = await facemesh.load({ inputResolution, scale: 0.8 });
+    setInterval(async () => {
+      await detect(net);
+    }, 100);
+  };
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = net.estimateFaces(video);
+    }
+  };
+  runFacemesh();
   return (
     <div>
-      <Webcam ref={webcamRef} style={fullScreenStyles} />
+      <Webcam
+        ref={webcamRef}
+        width={inputResolution.width}
+        height={inputResolution.height}
+      />
       <canvas ref={canvasRef} style={fullScreenStyles} />
     </div>
   );

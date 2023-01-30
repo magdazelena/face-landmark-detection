5ef9e34821f588dd0045f35fba6f623ddd2aa601
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-01-30 16:36:16 +0100

    refactor: switched to request animation frame and recursive detection

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Webcam from "react-webcam";
 import { drawMesh } from "./utils/drawMesh";
 
 const inputResolution = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: 1080,
+  height: 900,
 };
 const videoConstraints = {
   width: inputResolution.width,
@@ -29,31 +29,31 @@ function App() {
       model,
       detectorConfig
     );
-    setInterval(async () => {
-      await detect(detector);
-    }, 100);
-  };
-  const detect = async (net) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
 
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+    const detect = async (net) => {
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+      ) {
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
 
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
 
-      const estimationConfig = { flipHorizontal: false };
-      const faces = await net.estimateFaces(video, estimationConfig);
-      const ctx = canvasRef.current.getContext("2d");
-      drawMesh(faces[0], ctx);
-    }
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        const estimationConfig = { flipHorizontal: false };
+        const faces = await net.estimateFaces(video, estimationConfig);
+        const ctx = canvasRef.current.getContext("2d");
+        requestAnimationFrame(() => drawMesh(faces[0], ctx));
+        detect(detector);
+      }
+    };
+    detect(detector);
   };
   runDetector();
   return (
@@ -62,7 +62,7 @@ function App() {
         ref={webcamRef}
         width={inputResolution.width}
         height={inputResolution.height}
-        style={fullScreenStyles}
+        style={{ opacity: 0 }}
         videoConstraints={videoConstraints}
       />
       <canvas
